6047b7cd65e1ede00db3057af87cb6c9afbf8556
Nadim <mohamednadim.matoussi@esprit.tn>
2022-04-21 20:12:35 +0100

    second commit

--- script.js
+++ script.js
@@ -3,6 +3,8 @@ var ratingValues = document.getElementById("rating-container").children;
 for (var i=0;i<5;i++) {
     ratingValues[i].onclick = function (){
         value = this.innerHTML;
+        this.style.color="white";
+        this.style.backgroundColor="#7c8798";
         document.getElementById("rating-value").innerHTML=value;
 
     }
